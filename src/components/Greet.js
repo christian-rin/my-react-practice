fe504fce49c0ade1ff00013787632ef6927787d8
Christian Rinosa (MKT-AS-C) <christian_rinosa@trendmicro.com>
2025-12-16 09:24:02 +0800

    Create component 'Greet.js'

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import Reeact from 'react';
+
+// function Greet() {
+//     return <h1>Hello from Greet component!</h1>;
+// }
+
+const Greet = () => <h1>Hello from Greet component!</h1>;
+
+export default Greet;
