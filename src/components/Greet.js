dd102a185f613cc1121b122ad137edc0fe496b50
Christian Rinosa (MKT-AS-C) <christian_rinosa@trendmicro.com>
2026-01-08 14:52:05 +0800

    Tutorial 9: Props

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,9 +1,13 @@
-import Reeact from 'react';
+import React from 'react';
 
-// function Greet() {
-//     return <h1>Hello from Greet component!</h1>;
-// }
+const Greet = props => {
+    console.log(props);
+    return (
+        <div>
+            <h1>Hello {props.name} a.k.a. {props.heroName}</h1>
+            {props.children}
+        </div>
+    )
+}
 
-const Greet = () => <h1>Hello from Greet component!</h1>;
-
-export default Greet;
+export default Greet
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,8 @@
-import Reeact, { Component } from 'react';
+import React, { Component } from 'react';
 
 class Welcome extends Component {
     render () {
-        return <h1>Class component!</h1>;
+        return <h1>Welcome {this.props.name} a.k.a. {this.props.heroName}</h1>;
     }
 }
 
