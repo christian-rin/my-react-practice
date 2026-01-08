bb26ba1090469d2301ad275c9b15a64dfbd37784
Christian Rinosa (MKT-AS-C) <christian_rinosa@trendmicro.com>
2026-01-08 15:27:45 +0800

    Tutorial 10: state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
+      <Message />
+      {/* <Greet name="Bruce" heroName="Batman">
         This is children props
       </Greet>
       <Greet name="Clark" heroName="Superman">
@@ -16,7 +18,7 @@ function App() {
       <Greet name="Diana" heroName="Wonder Woman"/>
       <Welcome name="Clark" heroName="Superman"/>
       <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Diana" heroName="Wonder Woman"/>
+      <Welcome name="Diana" heroName="Wonder Woman"/> */}
       {/* <Hello /> */}
     </div>
   );
